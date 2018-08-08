d499bc0c3313bcc81ef51cb28c83d2d033bd227d
welissondias <welissondias@gmail.com>
2018-08-07 21:43:49 -0400

    test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Bora acabar com isso logo.")
+        self.assertRegex(result.data.decode(), "Bora acabar com isso logo!")
 
 
 if __name__ == "__main__":
